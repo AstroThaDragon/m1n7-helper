7a124218fabf95a1c1d750ad8cae064c43b5d501
Brandon Bachert <soaringinthestarz@gmail.com>
2026-05-04 16:11:40 -0400

    Removed links for images and put the gifs into images folder instead

--- tags.py
+++ tags.py
@@ -5,45 +5,45 @@
 
 tag_list = {
     "angery": "*angy deer noises*\nimages/angery.jpg",
-    "arrived": "https://tenor.com/bqwyH.gif",
+    "arrived": "images/arrived.gif",
     "astro": "The dragon himself!\nimages/astrocandy.gif",
-    "baldi": "Reminds me of someone...\nhttps://tenor.com/t18lxNGsmsR.gif",
+    "baldi": "Reminds me of someone...\nimages/baldi.gif",
     "banned": """
     # YOU'RE BANNED!!!
     
     -CaseOh
-    https://tenor.com/bfuQS.gif
+    images/banned.gif
     """,
-    "beepboopbop": "Beep boop bop?\nhttps://tenor.com/oymh3fhEQ6g.gif",
-    "bite": "*Nom*\nhttps://media.tenor.com/m/OfK5pY2Qu6oAAAAd/fox-bite.gif",
-    "blu": "The reason it's named that? For someone I know. Yes, this tag is for them\nhttps://media.tenor.com/xDn7MANyP6kAAAAM/oh-yay-im-a-sacrifice-sacrifice.gif",
-    "boykisser": "Are *you* a boykisser?\nhttps://tenor.com/nzJ6JX0kQch.gif",
-    "🧱": "https://tenor.com/kIwHdKlRQif.gif",
-    "dead": "Feelin' like this, bru\nhttps://media.tenor.com/Rnmhy4e3iZ0AAAAM/cartman-eric-cartman.gif",
-    "deadchat": "This will TOTALLY work. Trust me\nhttps://media.tenor.com/bgfw0qY4qtkAAAAM/dead-group-chat-waiting.gif",
+    "beepboopbop": "Beep boop bop?\nimages/beepboopbop.gif",
+    "bite": "*Nom*\nimages/bite.gif",
+    "blu": "The reason it's named that? For someone I know. Yes, this tag is for them\nimages/blu.gif",
+    "boykisser": "Are *you* a boykisser?\nimages/boykisser.gif",
+    "🧱": "images/brick.gif",
+    "dead": "Feelin' like this, bru\nimages/dead.gif",
+    "deadchat": "This will TOTALLY work. Trust me\nimages/deadchat.gif",
     "deletethis": "Just delete it.\nimages/deletethis.jpg",
     "desgostang": "Bro... that's *desgostang*\nimages/desgostang.jpg",
-    "dragonhug": "Sometimes we all need a hug. Especially from a dragon. Imagine the warmth!!\nhttps://media.tenor.com/YeqPfkN2FXoAAAAM/dragon-hug.gif",
-    "forgetthat": "That didn't happen.\nhttps://media.tenor.com/hTqHcjxGcgAAAAA1/forget-never-happened.webp",
-    "freaky": "Rock ON!\nhttps://media.tenor.com/-10nOQpa7vgAAAAM/spongebob-rock-on.gif",
+    "dragonhug": "Sometimes we all need a hug. Especially from a dragon. Imagine the warmth!!\nimages/dragonhug.gif",
+    "forgetthat": "That didn't happen.\nimages/forgetthat.gif",
+    "freaky": "Rock ON!\nimages/freaky.gif",
     "freakycat": "It's okay, don't worry\nimages/freakycat.jpg",
-    "handshake": "Hands down; the best handshake gif out there ngl\nhttps://media.tenor.com/WD6SIYMVIDwAAAA1/smiling-friends-smiling.webp",
-    "gm": "Good morning, chat! I hope you have a great day! 💜\nhttps://tenor.com/uTjMRZH7CPb.gif",
-    "gn": "Goodnight, chat. Have a good rest 💜\nhttps://tenor.com/keQI16MA0M3.gif",
+    "handshake": "Hands down; the best handshake gif out there ngl\nimages/handshake.gif",
+    "gm": "Good morning, chat! I hope you have a great day! 💜\nimages/gm.gif",
+    "gn": "Goodnight, chat. Have a good rest 💜\nimages/gn.gif",
     "hoopla": """
     Sounds like a lot of..-
     Hoopla!
     # HOOPLA!
     *clunk*
-    https://tenor.com/bCLZt.gif
+    images/hoopla.gif
     """,
-    "huzzah": "HUZZAH!\nhttps://media.tenor.com/fWKoJ3PPqNQAAAAM/huzzah-reaction.gif",
+    "huzzah": "HUZZAH!\nimages/huzzah.gif",
     "imtotallyfine": "I'm totally fine, don't worry!\nimages/imtotallyfine.jpg",
     "laughat": """
     HA! Look at that! Look what they did! Point and laugh!!
     
     -# use this as a joke. Don't be rude!
-    https://media.tenor.com/03sP1WgI4iAAAAA1/duck-laugh.webp
+    images/laughat.gif
     """,
     "list": """
     Here's our current tag list! 💜
@@ -55,15 +55,15 @@ tag_list = {
     -# no, I don't mean MalO herself, but someone who is *like* her! Perhaps you know who they are?
     images/MalO_nom.gif
     """,
-    "minigames": "https://media.tenor.com/WHKNFcRaYrcAAAAM/pressure-pressure-roblox.gif",
+    "minigames": "images/minigames.gif",
     "minion": "*Stare*\nimages/minion-stare.gif",
-    "nightflaid": "Nightflaid from RetroSpecter V2 FNF. She just *OOZES* cool, bro\nhttps://media.tenor.com/otTSpg30ZmEAAAA1/nightflaid.webp",
+    "nightflaid": "Nightflaid from RetroSpecter V2 FNF. She just *OOZES* cool, bro\nimages/nightflaid.gif",
     "pathetic": "# Pathetic.\nimages/pathetic.jpg",
     "personalspace": "images/personalspace.jpg",
-    "poyo": "-# poyo\nhttps://tenor.com/bXig0.gif",
+    "poyo": "-# poyo\nimages/poyo.gif",
     "punch": "images/punch.jpg",
-    "retro": "Low quality ahh dance\nhttps://media.tenor.com/3e6r37MxJO8AAAAm/retrospecter-dance.webp",
-    "screwit": "Sometimes we all feel this way tbh\nhttps://media.tenor.com/2xQVmSgb6L0AAAAM/south-park-stan-marsh.gif",
+    "retro": "Low quality ahh dance\nimages/retro.gif",
+    "screwit": "Sometimes we all feel this way tbh\nimages/screwit.gif",
     "sharklunge": """
     -# Dun-dun..
     Dun-dun...
@@ -76,8 +76,8 @@ tag_list = {
     "spunchgun": "Omg!!11!!1 SPUNCH WIT A GUN!?1?!\nimages/spunchgun.jpg",
     "spunchpop": "I miss the old SpongeBob pops, bro..\nimages/spunchpop.jpg",
     "squid": "images/squidward-explosion.gif",
-    "stinks": "You must stink, bro. Sorry to tell you.\nhttps://media.tenor.com/Hc8xlYrQNvYAAAAM/boooooo-this-guy-stinks.gif",
-    "tired": "Feeling like this rn?\nhttps://media.tenor.com/rAlDD3QTqw8AAAAM/spongebob-squarepants-tired.gif",
+    "stinks": "You must stink, bro. Sorry to tell you.\nimages/stinks.gif",
+    "tired": "Feeling like this rn?\nimages/tired.gif",
     "toothless": "The best dragon frfr\nimages/toothless.gif",
     "toopliss": "I love these goofy memes of Toothless so much\nimages/toopliss.gif",
     "trade": """
@@ -87,8 +87,8 @@ tag_list = {
     *Do you accept?*
     images/trade.jpg
     """,
-    "umbre": "Bros on a Roomba, just chillin'\nhttps://media.tenor.com/5lc_PybCMVEAAAAm/umbreon-spin.webp",
+    "umbre": "Bros on a Roomba, just chillin'\nimages/umbre.gif",
     "umbrecup": "How did he even manage to do that??\nimages/umbrecup.jpg",
-    "vox": "He's a lover of Vox frfr\nhttps://tenor.com/vYg7uf2uGWP.gif",
+    "vox": "He's a lover of Vox frfr\nimages/vox.gif",
     "wtf": "👁️\nimages/wtf.jpg"
 }
